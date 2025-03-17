e4835bbce2c0d0453b77baa050e3f6b05658e3fb
zhemchuzhnyi <a_reshkin@inbox.ru>
2025-03-17 22:57:30 +0300

    Update and Refactor Code

---
+++ src/animals/birds/IFlying.java
@@ -0,0 +1,4 @@
+package animals.birds;
+
+public interface IFlying {
+}
--- src/animals/pet/Cat.java
+++ src/animals/pet/Cat.java
@@ -1,4 +1,14 @@
 package animals.pet;
 
-public class Cat {
+import animals.Animal;
+import data.ColorData;
+
+public class Cat extends Animal {
+
+
+        public Cat(String name, int age, int weight, ColorData color) {
+            super(name, age, weight, color);
+    }
+
+
 }
